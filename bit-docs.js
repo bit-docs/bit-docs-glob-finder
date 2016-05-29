e37a1ac4a0930ab31b8dd8704c489bc64a10c048
Justin Meyer <justinbmeyer@gmail.com>
2016-05-29 15:20:34 -0500

    adding all the files

---
+++ bit-docs.js
@@ -0,0 +1,7 @@
+var globFinder = require("./index");
+var processJavaScript = require("./process/javascript");
+module.exports = function(bitDocs){
+    // register your tags
+    bitDocs.register("finder", globFinder);
+
+});
---
+++ test.js
@@ -0,0 +1,30 @@
+var finder = require("./index"),
+	assert = require("assert"),
+	path = require("path");
+
+
+describe("bid-docs-glob-finder",function(){
+
+	it("is able to ignore", function(done){
+		var fileEventEmitter = finder({
+			glob: {
+				pattern: "**/*.{js,md}",
+				cwd: path.join(__dirname,"test"),
+				ignore: "node_mods/**/*"
+			}
+		});
+		var matches = 0;
+		fileEventEmitter.on("match",function(src){
+			matches ++;
+			if(src.indexOf("node_mods") >= 0) {
+				assert.ok(false, "Got something that should have been ignored - "+src);
+			}
+
+		});
+		fileEventEmitter.on("end",function(){
+			assert.equal(matches, 4, "4 files found");
+			done();
+		});
+	});
+
+});

4e3912c5cbf43f98f3d3f3b9e209a4750eb1a8d4
Justin Meyer <justinbmeyer@gmail.com>
2016-06-07 12:37:06 -0500

    working bit-docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,5 +1,4 @@
 var globFinder = require("./index");
-var processJavaScript = require("./process/javascript");
 module.exports = function(bitDocs){
     // register your tags
     bitDocs.register("finder", globFinder);

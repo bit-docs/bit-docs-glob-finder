aeb9dc84c3c28cd89ed125936946be17cbed4d9a
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-05 10:49:37 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,3 +1,13 @@
+/**
+ * @module {function} bit-docs-glob-finder
+ * @parent plugins
+ *
+ * @description Finds files for passing to the bit-docs processor.
+ *
+ * @body
+ *
+ * TBD
+ */
 var globFinder = require("./index");
 module.exports = function(bitDocs){
     // register your tags
